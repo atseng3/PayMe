a136895bb0733a0a68b67efc41231f5a9c53f44b
Albert Tseng <atseng3@gmail.com>
2014-04-12 15:31:36 -0700

    users and sessions complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zenpay::Application.config.secret_token = '1cbef29800cd1c79eb2ac71b8d2eb303d5e77b3aea55765e711028f13eb27d6c26253ade5ea8dd28dcde2fffb91ce4cef890a92d2f259dbc593915209ad6368a'
