1137a2362051a358498b5e3d2ec5044ae8e6465b
Dean <dean.yang02@gmail.com>
2014-04-12 16:10:14 -0700

    added fields to user & validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ActiveRecord::Base
   
   validates :password_digest, :presence => { :message => "Password can't be blank"}
   validates :password, :length => { :minimum => 8, :allow_nil => true }
-  validates :f_name, :l_name, :email, :session_token, :presence => true
-  
-  after_initialize :ensure_session_token
+  validates :f_name, :l_name, :email, :session_token, :phone_number, :presence => true
+  validates :phone_number, :uniqueness => true, :length => { :is => 10 }
+  validates :email, :uniqueness => true, :format => /[\w\.%\+\-]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)/i
+
+  after_initialize :ensure_session_token, :sanitize_fields
   
   def self.find_by_credentials(email, password)
     user = User.find_by_email(email)
@@ -37,5 +39,10 @@ class User < ActiveRecord::Base
   private
     def ensure_session_token
       self.session_token ||= self.class.generate_session_token
+    end
+
+    def sanitize_fields
+      self.phone_number = self.phone_number.gsub(/[^0-9]/, "") if self.phone_number
+      self.email = self.email.downcase if self.email
     end
 end
